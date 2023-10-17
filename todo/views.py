276eaf1a84a71b854e164655320a428e8c93f90b
Frida010 <>
2023-10-17 11:29:45 +0000

    add

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def say_hello():
-    return HttpResponse("Hello!")
+
+
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
